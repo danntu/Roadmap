034a60e0ab92b9db9e48b3ca3d3f7f8c271f615f
DMyrzakanov <dan_ntu@mail.ru>
2023-08-18 10:55:06 +0600

    Single line comment

--- src/main/java/basic_syntax/geeksforgeeks/GFG.java
+++ src/main/java/basic_syntax/geeksforgeeks/GFG.java
@@ -3,5 +3,7 @@ package basic_syntax.geeksforgeeks;
 public class GFG {
     public static void main(String[] args) {
         System.out.println("GeeksforGeeks!");
+
+        //System.out.println("This is a comment"); // Single line comment
     }
 }
